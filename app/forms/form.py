ba1bd4bba074c0ea48e96110296f9830dda98ee2
MHoryd <michal.horyd@gmail.com>
2024-01-27 19:28:40 +0100

    added length validator to form

--- app/forms/form.py
+++ app/forms/form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class RSVP_Form(FlaskForm):
     Field1 = StringField(label="Gość",validators=[DataRequired()])
     Field2 = SelectField(label="Czy pojawisz/pojawicie się?",choices=['Tak','Nie'],validators=[DataRequired()])
-    Field3 = StringField(label="Daj/Dajcie proszę znać o ew. wykluczeniach pokarmowych")
+    Field3 = StringField(label="Daj/Dajcie proszę znać o ew. wykluczeniach pokarmowych", validators=[Length(max=255)])
     Field4 = SelectField(label="Danie główne",choices=['Ryba','Mięso','Wege'],validators=[DataRequired()])
