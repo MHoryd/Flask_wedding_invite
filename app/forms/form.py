0dd3a416229ba7ac5d00dceb09cb74b6bd53377a
MHoryd <michal.horyd@gmail.com>
2024-01-27 10:39:05 +0100

    expanded form, refactored handling of form data so no duplicates will be created

--- app/forms/form.py
+++ app/forms/form.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField,SelectField
 from wtforms.validators import DataRequired
 
 
 class RSVP_Form(FlaskForm):
-    Pole1 = StringField(label="Gość",validators=[DataRequired()])
-    Pole2 = StringField(label="Pole2",validators=[DataRequired()])
-    Pole3 = StringField(label="Pole3",validators=[DataRequired()])
+    Field1 = StringField(label="Gość",validators=[DataRequired()])
+    Field2 = SelectField(label="Czy pojawisz/pojawicie się?",choices=['Tak','Nie'],validators=[DataRequired()])
+    Field3 = StringField(label="Daj/Dajcie proszę znać o ew. wykluczeniach pokarmowych")
+    Field4 = SelectField(label="Danie główne",choices=['Ryba','Mięso','Wege'],validators=[DataRequired()])
--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@ from app.main import bp
 from flask import render_template, request, jsonify
 from app.forms.form import RSVP_Form
 from app.main.mail import Email_notifi
-import json, os, ast
+import json, os, ast, datetime
 
 
 @bp.route('/',methods=['GET'])
@@ -12,18 +12,25 @@ def index():
     return render_template('index.html', form=form, no_guests=no_guests)
 
 
-@bp.route('/<int:url_num>', methods=['GET'])
-def index_guests(url_num):
+@bp.route('/<url_str>', methods=['GET'])
+def index_guests(url_str):
     guest_url_dict = ast.literal_eval(os.environ.get("guest_url_dict"))
-    second_guest = None
-    second_guest = None
     form = RSVP_Form()
-    for url, names_list in guest_url_dict.items():
-        if int(url) == url_num:
-            first_guest = names_list[0]
-            second_guest = names_list[1]
-    form.Pole1.data = f"{first_guest} oraz {second_guest}"
-    return render_template('index.html', first_guest=first_guest,second_guest=second_guest, form=form)
+    guest,one_guest,two_guests,multiple_guests,no_guests = None,None,None,None,None
+    visiting_guests = [guest for key, guests in guest_url_dict.items() if str(key) == url_str for guest in guests]
+    if len(visiting_guests) == 1:
+        one_guest = True
+        guest = visiting_guests[0]
+        form.Field1.data = f"{guest}"
+    elif len(visiting_guests) == 2:
+        two_guests = visiting_guests
+        form.Field1.data = f"{visiting_guests[0]} oraz {visiting_guests[1]}"
+    elif len(visiting_guests) > 2:
+        multiple_guests=visiting_guests
+        form.Field1.data = f"{','.join(visiting_guests)}"
+    else:
+        no_guests = True 
+    return render_template('index.html',form=form,guest=guest,one_guest=one_guest,two_guests=two_guests,multiple_guests=multiple_guests,no_guests=no_guests)
 
 @bp.route('/process_form', methods=['POST'])
 def process_form():
@@ -33,22 +40,40 @@ def process_form():
         with open("answers.json", 'r+') as file:
             existing_data = json.load(file)
             answer = {
-                "Goście":form.data.get("Pole1"),
-                "Pole2":form.data.get("Pole2"),
-                "Pole3":form.data.get("Pole3")
+                "Guest":form.data.get("Field1"),
+                "Edited":False,
+                "Datetime":str(datetime.datetime.now()),
+                "Answers":
+                {
+                "Field2":form.data.get("Field2"),
+                "Field3":form.data.get("Field3"),
+                "Field4":form.data.get("Field4")
+                }
             }
-            for i in existing_data:
-                if i == answer:
+            for index, dict in enumerate(existing_data):
+                if dict['Guest'] == answer['Guest'] and dict['Answers'] == answer['Answers']:
                     error_msg = "Już mam tą odpowiedź ;)"
                     return jsonify({'message':error_msg,"success": False})
+                elif dict['Guest'] == answer['Guest']:
+                    success_msg = "Dzięki, wszystko się udało, zmieniłem odpowiedź :)"
+                    existing_data[index]['Answers']['Field2'] = answer["Answers"]["Field2"]
+                    existing_data[index]['Answers']['Field3'] = answer["Answers"]["Field3"]
+                    existing_data[index]['Answers']['Field4'] = answer["Answers"]["Field4"]
+                    existing_data[index]["Edited"] = True
+                    existing_data[index]["Datetime"] = str(datetime.datetime.now())
+                    file.seek(0)
+                    json.dump(existing_data, file, indent=2)
+                    file.truncate()
+                    email_object = Email_notifi(answer)
+                    email_object.send_message()
+                    return jsonify({'message': success_msg, "success": True })
             existing_data.append(answer)
             file.seek(0)
             json.dump(existing_data, file, indent=2)
             file.truncate()
-            print(answer)
             email_object = Email_notifi(answer)
             email_object.send_message()
-        return jsonify({'message': success_msg, "success": True })
+            return jsonify({'message': success_msg, "success": True })
     else:
         error_msg = "Nie wszystkie pola zostały uzupełnione"
         return jsonify({'message':error_msg,"success": False})
